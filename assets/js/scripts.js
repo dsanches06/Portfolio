1cab9eb279e0c695c851ba49f3ddce7c6d5b8b5b
Danilson Jorge Tavares Sanches <dsanches06@outlook.com>
2025-12-21 01:51:26 +0000

    fix: Menu Mobile, responsividade, e alinhar header e nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,24 @@
-const btn = document.querySelector('.menu-toggle');
-const menu = document.querySelector('nav ul');
+/* script para menu responsivo */
+const btn = document.querySelector(".menu-toggle");
+const menu = document.querySelector("nav ul");
+const main = document.querySelector("main");
 
 // Ao clicar no botão, liga/desliga a classe 'aberto'
-btn.addEventListener('click', () => {
-    menu.classList.toggle('aberto');
+// e adiciona o padding do carousel
+btn.addEventListener("click", () => {
+    addClassList(menu);
+    toogleSlideMenu(menu);
 });
+
+function addClassList(element) {
+    element.classList.toggle("aberto");
+}
+
+function toogleSlideMenu(element) {
+    if (element.classList.contains('aberto')) {
+        main.style.padding = "10rem 1rem";
+    }
+    else {
+        main.style.padding = "2.5rem 1rem";
+    }
+}
