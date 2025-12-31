0a14c0d3324d4329e3d85d5d83ef8c5d6c6098d1
Danilson Jorge Tavares Sanches <dsanches06@outlook.com>
2025-12-31 21:32:14 +0000

    feat: finalizando portfolio

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const urls = {
   linkedin: "https://www.linkedin.com/in/danilson-sanches/",
   github: "https://github.com/dsanches06",
+  phone: "tel:+351123456789",
+  outlook: "mailto:dsanches06@outlook.com"
 };
 
 /**
@@ -25,45 +27,72 @@ const getElementById = (id) => document.getElementById(id);
  * para abrir em uma nova aba (target="_blank") e adiciona um listener de clique
  * que registra no console a URL do elemento quando clicado.
  */
-const setupLink = (element, url) => {
+const setupLink = (element, url, type) => {
   if (element) {
-    element.setAttribute("href", url);
-    element.setAttribute("target", "_blank");
-    element.addEventListener("click", () => {
-      console.log("Clicou no link:", url);
-    });
+
+    switch (type) {
+      case "link":
+        element.setAttribute("href", url);
+        element.setAttribute("target", "_blank");
+        break;
+      case "phone":
+      case "email":
+        element.setAttribute("href", url);
+        break;
+    }
+
+    console.log(url);
   }
 };
 
 // Obtém os elementos DOM dos links
-let linkedin = getElementById("linkedin");
-let github = getElementById("github");
+const linkedin = getElementById("linkedin");
+const github = getElementById("github");
+const phone = getElementById("phone");
+const outlook = getElementById("outlook");
+
 
 // Configura os links com as URLs apropriadas
-setupLink(linkedin, urls.linkedin);
-setupLink(github, urls.github);
+setupLink(linkedin, urls.linkedin, "link");
+setupLink(github, urls.github, "link");
+
+//
+setupLink(phone, urls.phone, "phone");
+setupLink(outlook, urls.outlook, "email");
 
 /*************************************************** */
-const btn = document.querySelector('.menu-toggle');
-const menu = document.querySelector('nav ul');
-const main = document.querySelector("main");
-
+/* script para menu responsivo */
+const btn = document.querySelector(".menu-toggle");
+const link_container = document.querySelector(".link-container");
 
 // Ao clicar no botão, liga/desliga a classe 'aberto'
-btn.addEventListener('click', () => {
-  addClassList(menu);
-  toogleSlideMenu(menu);
+// e adiciona o padding do carousel
+btn.addEventListener("click", () => {
+  link_container.classList.toggle("aberto");
 });
 
-function addClassList(element) {
-  element.classList.toggle("aberto");
+/*************************************************** */
+//Interatividade (JavaScript) para receber dados preenchidos no form
+
+function enviarForm(event) {
+  event.preventDefault();
+
+  const nome = getElementById("nome").value;
+  const email = getElementById("email").value;
+  const assunto = getElementById("assunto").value;
+  const mensagem = getElementById("mensagem").value;
+
+  //mostra o banner
+  document.querySelector(".banner-info").style.display = "flex";
+  document.querySelector(".banner-info-p").firstElementChild.innerHTML = "Obrigado por me contactar " + nome + " em breve, irei responder.";
 }
 
-function toogleSlideMenu(element) {
-  if (element.classList.contains('aberto')) {
-    main.style.padding = "10rem 1rem";
-  }
-  else {
-    main.style.padding = "1.5rem 1rem";
-  }
-}
+/*************************************************************/
+//fechar banner, depois de clicar em cima
+const banner_info = document.querySelector(".banner-info");
+banner_info.addEventListener("click", () => {
+  //fecha o banner
+  banner_info.style.display = "none";
+  //limpa o formulario
+  getElementById("contact--form").reset();
+});
